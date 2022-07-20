863c9231b393b0ba9176dd6406f0027d84594c98
Silvia Trejo <s.y.trejo26@gmail.com>
2022-07-19 21:46:25 -0400

    Adding more details to the structure.

--- carDealership/src/carDealership/Customer.java
+++ carDealership/src/carDealership/Customer.java
@@ -2,11 +2,41 @@ package carDealership;
 
 public class Customer {
 	
-	String name;
-	String address;
-	double cashOnHand;
+	private String name;
+	private String address;
+	private double cashOnHand;
 	
 	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public double getCashOnHand() {
+		return cashOnHand;
+	}
+
+
+	public void setCashOnHand(double cashOnHand) {
+		this.cashOnHand = cashOnHand;
+	}
+
+
 	public void purchaseCar(Vehicle veicle, Employee emp, boolean finance) {
 		
 	}
---
+++ carDealership/src/carDealership/Employee.java
@@ -0,0 +1,5 @@
+package carDealership;
+
+public class Employee {
+
+}
