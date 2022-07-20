e84b7615c902b179ddcdcbe1edf8d775035d8b49
Silvia Trejo <s.y.trejo26@gmail.com>
2022-07-19 21:48:01 -0400

    creating a customer class and instantiating the class

--- carDealership/src/carDealership/Dealership.java
+++ carDealership/src/carDealership/Dealership.java
@@ -6,9 +6,9 @@ public class Dealership {
 		
 		Customer cust1 = new Customer();
 		
-		cust1.name = "Tom";
-		cust1.address = "123 Here St.";
-		cust1.cashOnHand = 12000.00;
+		cust1.setName("Tom");
+		cust1.setAddress("123 Here St.");
+		cust1.setCashOnHand(12000.00);
 		
 		cust1.purchaseCar(vehicle,  emp, false);
 
