d5fcc66998ec7b1ed3677b752b3a0e6d20b58021
Silvia Trejo <s.y.trejo26@gmail.com>
2022-07-19 21:42:37 -0400

    stubbing out the car dealership relationships

---
+++ carDealership/src/carDealership/Customer.java
@@ -0,0 +1,14 @@
+package carDealership;
+
+public class Customer {
+	
+	String name;
+	String address;
+	double cashOnHand;
+	
+	
+	public void purchaseCar(Vehicle veicle, Employee emp, boolean finance) {
+		
+	}
+
+}
--- carDealership/src/carDealership/Dealership.java
+++ carDealership/src/carDealership/Dealership.java
@@ -4,6 +4,13 @@ public class Dealership {
 
 	public static void main(String[] args) {
 		
+		Customer cust1 = new Customer();
+		
+		cust1.name = "Tom";
+		cust1.address = "123 Here St.";
+		cust1.cashOnHand = 12000.00;
+		
+		cust1.purchaseCar(vehicle,  emp, false);
 
 	}
 
